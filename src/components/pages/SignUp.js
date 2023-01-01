98a2dc3194ad6e5abca682732d76b1ca2eedb991
Krish Shah <2003kshah@gmail.com>
2023-01-01 15:53:18 -0500

    first

---
+++ src/components/pages/SignUp.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../App.css';
+import Footer from '../Footer';
+
+export default function SignUp() {
+  return (
+    <>
+      <h1 className='products'>SIGN UP</h1>
+      <Footer />
+    </>
+  );
+}
